47d799fcbc137f096c9f2b151d5c4fd71a4a6521
YUNMI LEE <goya3014@hanmail.net>
2024-03-11 16:09:22 +0900

    회원 리포지토리 & 서비스 개발

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -10,4 +10,13 @@ public class Address {
     private String city;
     private String street;
     private String zipcode;
+
+    protected Address() { //JPA 스펙상 기본 생성자 필수
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
