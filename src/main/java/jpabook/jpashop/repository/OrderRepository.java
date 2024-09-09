c32e39b3f27649905ca11e0323ac7f286a84eb3c
YUNMI LEE <goya3014@hanmail.net>
2024-09-09 22:45:23 +0900

    페치조인 최적화

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -121,4 +121,11 @@ public class OrderRepository {
         }
         return member.name.like(nameCond);
     }*/
+
+    public List<Order> findAllWithMemberDelivery() {
+        return em.createQuery("select o from Order o" +
+                        " join fetch o.member m" +
+                        " join fetch o.delivery d", Order.class
+                ).getResultList();
+    }
 }
